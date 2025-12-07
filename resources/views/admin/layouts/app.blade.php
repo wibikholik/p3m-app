950bb5bd6d01af27a2185be883c6e4e02ad3b692
Wibii <edukasikulaih7@gmail.com>
2025-12-07 19:11:26 +0700

    perbaikan

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,60 +4,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Dashboard Admin P3M')</title>
+    
+    {{-- Tailwind CSS (Dipanggil lebih dulu) --}}
     <script src="https://cdn.tailwindcss.com"></script>
-    <!-- plugins:css -->
+    
     <link rel="stylesheet" href="{{ asset('assets/vendors/simple-line-icons/css/simple-line-icons.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icons.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css') }}">
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
     <link rel="stylesheet" href="{{ asset('assets/vendors/font-awesome/css/font-awesome.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/jvectormap/jquery-jvectormap.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/daterangepicker/daterangepicker.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/chartist/chartist.min.css') }}">
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
-    <!-- endinject -->
-    <!-- Layout styles -->
     <link rel="stylesheet" href="{{ asset('assets/css/vertical-light-layout/style.css') }}">
-    <!-- End layout styles -->
-<<<<<<< HEAD
-=======
-    <!-- Sidebar Menu Styles -->
     <link rel="stylesheet" href="{{ asset('assets/css/sidebar-menu.css') }}">
-    <!-- End sidebar menu styles -->
->>>>>>> refs/remotes/origin/main
+    
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
     @yield('extra-css')
 </head>
-<body class="bg-gray-100 flex min-h-screen">
 
-    {{-- Sidebar --}}
-    @include('admin.layouts.sidebar')
+{{-- Menggunakan class bawaan template 'container-scroller' dan 'sidebar-fixed' untuk layout --}}
+<body class="bg-gray-100 container-scroller sidebar-fixed"> 
 
-    {{-- Bagian kanan (konten utama + navbar + footer) --}}
-    <div class="flex-1 flex flex-col">
+    {{-- BARU: CONTAINER UTAMA --}}
+    <div id="wrapper" class="flex min-h-screen"> 
 
-        {{-- Navbar --}}
-        @include('admin.layouts.navbar')
+        {{-- Sidebar --}}
+        @include('admin.layouts.sidebar')
 
-        {{-- Konten Utama --}}
-        <main class="flex-1 p-6">
-            @yield('content')
-        </main>
+        {{-- Main Wrapper for Navbar and Content (Tailwind structure is kept, but adjusted) --}}
+        <div class="main-panel flex-1 flex flex-col"> 
 
-        {{-- Footer --}}
-        @include('admin.layouts.footer')
+            {{-- Navbar --}}
+            @include('admin.layouts.navbar')
 
-        @stack('scripts')
+            {{-- Konten Utama --}}
+            <div class="content-wrapper flex-grow p-6"> 
+                @yield('content')
+            </div>
+
+            {{-- Footer --}}
+            @include('admin.layouts.footer')
+
+        </div>
     </div>
 
-        <!-- container-scroller -->
-    <!-- plugins:js -->
+
+    {{-- Vendor JS (Harus diletakkan di akhir body) --}}
     <script src="{{ asset('assets/vendors/js/vendor.bundle.base.js') }}"></script>
-    <!-- endinject -->
-    <!-- Plugin js for this page -->
+    
     <script src="{{ asset('assets/vendors/chart.js/chart.umd.js') }}"></script>
     <script src="{{ asset('assets/vendors/jvectormap/jquery-jvectormap.min.js') }}"></script>
     <script src="{{ asset('assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
@@ -67,23 +62,18 @@
     <script src="{{ asset('assets/vendors/chartist/chartist.min.js') }}"></script>
     <script src="{{ asset('assets/vendors/progressbar.js/progressbar.min.js') }}"></script>
     <script src="{{ asset('assets/js/jquery.cookie.js') }}"></script>
-    <!-- End plugin js for this page -->
-    <!-- inject:js -->
+    
     <script src="{{ asset('assets/js/off-canvas.js') }}"></script>
     <script src="{{ asset('assets/js/hoverable-collapse.js') }}"></script>
     <script src="{{ asset('assets/js/misc.js') }}"></script>
     <script src="{{ asset('assets/js/settings.js') }}"></script>
     <script src="{{ asset('assets/js/todolist.js') }}"></script>
-    <!-- endinject -->
-    <!-- Custom js for this page -->
+    
     <script src="{{ asset('assets/js/dashboard.js') }}"></script>
-    <!-- End custom js for this page -->
-<<<<<<< HEAD
-=======
-    <!-- Sidebar Menu Script -->
+    
     <script src="{{ asset('assets/js/sidebar-menu.js') }}"></script>
-    <!-- End sidebar menu script -->
->>>>>>> refs/remotes/origin/main
+    
+    @stack('scripts')
     @yield('extra-js')
 </body>
 </html>
